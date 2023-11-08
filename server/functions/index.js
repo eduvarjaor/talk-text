fb0757ee6bfc5c29be7f6c335be68ff657b885f3
eduvarjaor <eduardovarjaor@gmail.com>
2023-11-07 21:26:03 -0300

    feat(Server): Firebase credentials

--- server/functions/index.js
+++ server/functions/index.js
@@ -1,19 +1,6 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+const functions = require("firebase-functions");
+const { admin } = require("./src/config/firebaseConfig");
 
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
-
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
-
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.sendAudio = functions.https.onRequest(async (req, res) => {
+    res.status(200).send("OK");
+});
